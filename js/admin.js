0d2a2a1933f8412aa327fed8d48dcf55e5eb8280
asifmmdv <mamedov_asif@icloud.com>
2025-04-28 23:04:44 +0400

    fix

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,21 @@
+function verify(){
+   const token = localStorage.getItem("token") 
+   const status = localStorage.getItem("status") 
+
+   if(!token || !status){
+    window.location.href="/auth/login.html"
+   }
+   else{
+    fetch ("https://neptunbk.vercel.app/auth/verify-token",{
+        headers:{
+            "Authorization" : `Bearer ${token}`
+        }
+    }).then(res => res.json())
+      .then(data => console.log(data))
+   }
+}
+verify()
+
 const data = []
 const modal = document.getElementById('modal')
 const table = document.getElementById('table')
---
+++ js/login.js
@@ -0,0 +1,27 @@
+const email = document.getElementById('email-input')
+const password = document.getElementById('password-input')
+
+function handleLogin(){
+    const emailVal = email.value
+    const passwordVal = password.value
+
+    fetch("https://neptunbk.vercel.app/auth/login",{
+        method : "POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            login : emailVal,
+            password: passwordVal
+        })
+    }).then(res => res.json())
+      .then(mel => {
+        const { refresh,token,status } = mel
+
+        if(status){
+            localStorage.setItem("token", token)
+            localStorage.setItem("status", status)
+            location.href = "/admin/admin.html"
+        }
+      })
+}
